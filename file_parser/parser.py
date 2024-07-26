5595bb8c6157ac49b9677e59394eaa8ee2c21ac7
Becky Robb <10777482+tabularasa7@users.noreply.github.com>
2024-07-25 23:25:06 -0600

    initial work on csv file parser

---
+++ file_parser/parser.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import csv
+import os
+
+with open(os.path.dirname(__file__) + '/../84-1321373_centennial-healthcare-plaza_standardcharges.csv') as csv_file:    
+    reader = csv.reader(csv_file)
+
+    # update = next(reader)
+
+    # baseHeader = next(reader)
+    # print(update)
+    # print(baseHeader)
+
+    prices = []
+    split_prices = []
+
+    # prevRow = reader[0]
+    for row in reader:
+        if row[0] in (None, ""):
+            prices.append(split_prices)
+            split_prices = []
+            # print(row)
+            # print(next(reader))
+            # print(next(reader))
+        else:
+            split_prices.append(row)
+        # else:
+        #     print(len(prices))
+        #     newFile = open(os.path.dirname(__file__) + '/../84-1321373_centennial-healthcare-plaza_standardcharges' + prices[0] + '.csv', 'w+')
+        #     newFile.writelines(prices)
+        #     prices = []
+
+    print(len(prices))
+    print(prices[0][0])
+
+
+# results.close()
